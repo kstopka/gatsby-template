f5490472bfd7a37467cdb25102656d8e44461997
Krystian Stopka <kr6stopka@gmail.com>
2024-03-29 12:28:31 +0100

    added create pages, init home page

---
+++ gatsby-custom/queries/standard/index.ts
@@ -0,0 +1,30 @@
+import { CreatePagesArgs } from "gatsby";
+import { IAllWpPage } from "./types";
+
+class StandardResourceClass {
+  getPages = async (graphql: CreatePagesArgs[`graphql`]) => {
+    const {
+      data,
+    }: {
+      errors?: any;
+      data?: IAllWpPage;
+    } = await graphql(`
+      {
+        allWpPage {
+          nodes {
+            id
+            slug
+            title
+            content
+          }
+        }
+      }
+    `);
+
+    return data?.allWpPage.nodes;
+  };
+}
+
+const StandardResource = new StandardResourceClass();
+
+export default StandardResource;
---
+++ src/types/standard/index.d.ts
@@ -0,0 +1,6 @@
+export interface IPage {
+  id: string;
+  slug: string;
+  title: string;
+  content: string;
+}
